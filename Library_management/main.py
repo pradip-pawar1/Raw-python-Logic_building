58756ec0bbeb10b952faedb6b12edc348b7c0e1b
Pradip <pradip.codelabplus@gmail.com>
2025-10-14 21:44:30 +0530

    Added login system and usernmes who are users of library

--- Library_management/main.py
+++ Library_management/main.py
@@ -19,19 +19,43 @@ def return_book():
 def exit():
     raise SystemExit("System closed..!")
 
-print(f"1. View avaliable Books")
-print(f"2. Borrow Book")
-print(f"3. Return Book")
-print(f"4. Exit\n")
+def login(users, username):
+    return_key = False
+    for i in range(len(users)):
+        # print(i)
+        if username == users[i]:
+            print(f"User verified!")
+            return_key = True
+            break
+    else:
+        print(f"User not verified.")
+    return return_key
+
+def show_details(login_key):
+    if login_key == True:
+        print(f"1. View avaliable Books")
+        print(f"2. Borrow Book")
+        print(f"3. Return Book")
+        print(f"4. Exit\n")
+    else:
+        print("User is new or not verified!")
+        exit()
+
+users = ["Pradip", "Rahul", "Rohit", "Rani", "Ram", "Sita"]
+user_name = input("Enter your username: ")
+key = login(users=users, username=user_name)
+
+show_details(key)
 
 choise = int(input("Enter your choise: "))
 
-match choise:
-    case 1:
-        view_books(books= books)
-    case 2:
-        borrow_books()
-    case 3:
-        return_book()
-    case 4:
-        exit()
+while True:
+    match choise:
+        case 1:
+            view_books(books= books)
+        case 2:
+            borrow_books()
+        case 3:
+            return_book()
+        case 4:
+            exit()
